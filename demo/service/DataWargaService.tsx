e2b420afd5661ce598dfc66a79a419587309ad76
herusukma <herusukmafama@gmail.com>
2024-03-28 11:37:46 +0700

    Update getList error handling

--- demo/service/DataWargaService.tsx
+++ demo/service/DataWargaService.tsx
@@ -2,8 +2,24 @@ import { Demo } from '@/types';
 
 export const DataWargaService = {
     getDataWarga() {
-        return fetch('https://localhost:44313/idcen/DataWarga/getList')
-            .then((res) => res.json())
-            .then((d) => d.data as Demo.Customer[]);
+        const url = "https://localhost:44313/idcen/DataWarga/getList";
+        // return fetch('https://localhost:44313/idcen/DataWarga/getList')
+        //     .then((res) => res.json())
+        //     .then((d) => d.data as Demo.Customer[]);
+
+        // fetch with error handling
+        return fetch(url).then((response) => {
+                if (response.ok) {
+                  return response.json();
+                }
+                throw new Error('Something went wrong');
+              })
+              .then((responseJson) => {
+                // Do something with the response
+                return responseJson.data as Demo.Customer[]
+              })
+              .catch((error) => {
+                console.log(error);
+              });
     }
 };
